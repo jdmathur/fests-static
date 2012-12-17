7bb9457cc11bb35f40c88a0385ee4a22f501844a
Jaiditya <jaiditya.mathur@gmail.com>
2012-12-18 02:22:05 +0530

    Static files changed
    Frontend of KY has been added

---
+++ static/js/float-item.js
@@ -0,0 +1,40 @@
+
+//float control of floating items
+
+$(document).ready(function() {
+$('#event1bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#event2bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#event3bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#event4bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#event5bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#sponsor').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+$('#event6bubble').jqFloat({
+  width: 20,
+  height: 20,
+  speed: 1000
+});
+});
