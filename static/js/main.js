0e8f4971345482e5c8ae91bdcfaa17d8acbb0dc6
Jaiditya <jaiditya.mathur@gmail.com>
2012-12-21 14:50:08 +0530

    apiURL changed

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-var apiUrl = 'http://pure-fjord-9921.herokuapp.com/api/';
+var apiUrl = 'http://pure-fjord-9921.herokuapp.com/api/vi';
 var Events = null, Pages = null, Sections = null, sView = null;
 
 function slugify(text) {
