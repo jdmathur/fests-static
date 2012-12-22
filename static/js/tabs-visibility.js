940d1baec6b00ceab709d4fd7ff88f2b1dfc09d9
Jaiditya <jaiditya.mathur@gmail.com>
2012-12-23 03:40:13 +0530

    tabular layout

---
+++ static/js/tabs-visibility.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+            $("#natraj").click(function() {
+                //need the following to toggle
+                $("#natraj1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#bandish").click(function() {
+                //need the following to toggle
+                $("#bandish1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#abhinay").click(function() {
+                //need the following to toggle
+                $("#abhinay1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#toolika").click(function() {
+                //need the following to toggle
+                $("#toolika1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#crosswindz").click(function() {
+                //need the following to toggle
+                $("#crosswindz1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#samvaad").click(function() {
+                //need the following to toggle
+                $("#samvaad1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#enquizta").click(function() {
+                //need the following to toggle
+                $("#enquizta1").css("display", "block");    
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#cine-events").click(function() {
+                //need the following to toggle
+                $("#cine-event1").css("display", "block");
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#pronites").click(function() {
+                //need the following to toggle
+                $("#pronites1").css("display", "block");
+            });
+        });
+        
+        $(document).ready(function(){
+            $(".close").click(function() {
+                //need the following to toggle
+                $("#pronites1, #cine-event1, #enquizta1, #samvaad1, #crosswindz1, #toolika1, #abhinay1, #bandish1, #natraj1").css("display", "none");
+            });
+        });
