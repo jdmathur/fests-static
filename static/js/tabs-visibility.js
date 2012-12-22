269829141ab1f7eac09e47cac7bab5aa8dca0488
Jaiditya <jaiditya.mathur@gmail.com>
2012-12-23 04:11:26 +0530

    header changed test

--- static/js/tabs-visibility.js
+++ static/js/tabs-visibility.js
@@ -1,49 +1,56 @@
 $(document).ready(function(){
             $("#natraj").click(function() {
                 //need the following to toggle
-                $("#natraj1").css("display", "block");    
+                $("#natraj1").css("display", "block");
+                $("#myModalLabel").html("Natraj");
             });
         });
         
         $(document).ready(function(){
             $("#bandish").click(function() {
                 //need the following to toggle
-                $("#bandish1").css("display", "block");    
+                $("#bandish1").css("display", "block");
+                $("#myModalLabel").html("Bandish");                
             });
         });
         
         $(document).ready(function(){
             $("#abhinay").click(function() {
                 //need the following to toggle
-                $("#abhinay1").css("display", "block");    
+                $("#abhinay1").css("display", "block");
+                $("#myModalLabel").html("Abhinay");                
             });
         });
         
         $(document).ready(function(){
             $("#toolika").click(function() {
                 //need the following to toggle
-                $("#toolika1").css("display", "block");    
+                $("#toolika1").css("display", "block");
+                $("#myModalLabel").html("Toolika");                
             });
         });
         
         $(document).ready(function(){
             $("#crosswindz").click(function() {
                 //need the following to toggle
-                $("#crosswindz1").css("display", "block");    
+                $("#crosswindz1").css("display", "block");
+                $("#myModalLabel").html("Crosswindz");                
             });
         });
         
         $(document).ready(function(){
             $("#samvaad").click(function() {
                 //need the following to toggle
-                $("#samvaad1").css("display", "block");    
+                $("#samvaad1").css("display", "block");
+                $("#myModalLabel").html("Samvaad");                
             });
         });
         
         $(document).ready(function(){
             $("#enquizta").click(function() {
                 //need the following to toggle
-                $("#enquizta1").css("display", "block");    
+                $("#enquizta1").css("display", "block");
+                $("#myModalLabel").html("Enquizta");                
             });
         });
         
@@ -51,6 +58,7 @@ $(document).ready(function(){
             $("#cine-events").click(function() {
                 //need the following to toggle
                 $("#cine-event1").css("display", "block");
+                $("#myModalLabel").html("Cine-Events");
             });
         });
         
@@ -58,6 +66,7 @@ $(document).ready(function(){
             $("#pronites").click(function() {
                 //need the following to toggle
                 $("#pronites1").css("display", "block");
+                $("#myModalLabel").html("Pronites");
             });
         });
         
@@ -65,6 +74,7 @@ $(document).ready(function(){
             $("#sponsors").click(function() {
                 //need the following to toggle
                 $("#sponsors1").css("display", "block");
+                $("#myModalLabel").html("Sponsors");
             });
         });
         
